a1b2514436853f1fab6c232ac2598a7b8b227fbd
ramseyFHSU <ramsey.fhsu@gmail.com>
2023-04-19 16:20:33 -0500

    Week 13

---
+++ Week 13/controller/registerController.js
@@ -0,0 +1,32 @@
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
+
+const handelNewUSer = async (req, res) => {
+  const user = req.body.user;
+  const pwd = req.body.password;
+
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: "Username and password is required" });
+  // check for duplicate username in the DB
+  const duplicate = await User.findOne({ username: user }).exec();
+  if (duplicate) return res.sendStatus(409); //Conflict
+
+  try {
+    // encrypt the password
+    const hashPwd = await bcrypt.hash(pwd, 10);
+
+    // create and store the new user
+    const result = await User.create({
+      username: user,
+      password: hashPwd,
+    });
+    console.log(result);
+    res.status(201).json({ success: `New User ${user} is created!` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { handelNewUSer };
